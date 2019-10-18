e6bf6eb398a33323268a9385193e2e5396066733
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-18 17:38:17 +0300

    Portfolio added

--- wp-content/themes/cln/footer.php
+++ wp-content/themes/cln/footer.php
@@ -4,13 +4,12 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1 text-center">
-                <p>&copy; STS. All Rights Reserved. <br>Created by <a href="http://smarttsoft.com/" target="_blank">SmartSoft</a> Images: <a href="http://pexels.com/" target="_blank">Pexels</a></p>
+                <p>&copy; STS. All Rights Reserved. <br>Created by <a href="http://smarttsoft.com/" target="_blank">SmartSoft</a> Images: <a href="https://picsum.photos/" target="_blank">Lorem Picsum</a></p>
             </div>
         </div>
     </div>
 </footer>
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
