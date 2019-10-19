295ab36a3c34612fb094d68a4a8210ebc6a3b74c
GalchenkoIurii <34748274+GalchenkoIurii@users.noreply.github.com>
2019-10-19 23:26:12 +0300

    Customized icons, added link to icon, changed styles of icons

--- wp-content/themes/cln/framework-customizations/extensions/shortcodes/shortcodes/icon/options.php
+++ wp-content/themes/cln/framework-customizations/extensions/shortcodes/shortcodes/icon/options.php
@@ -11,5 +11,12 @@ $options = array(
 		'type'  => 'text',
 		'label' => __( 'Title', 'fw' ),
 		'desc'  => __( 'Icon title', 'fw' ),
-	)
+	),
+
+    // Custom options
+    'url' => array(
+        'label' => __('Url', 'cln'),
+        'desc'  => __('Insert the URL of icon', 'cln'),
+        'type'  => 'text',
+    ),
 );
